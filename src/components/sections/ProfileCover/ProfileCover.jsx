2bfde1b097f15882ad0afb2389c43815d1825b69
madhukarreddyganga <45326658+madhukarreddyganga@users.noreply.github.com>
2023-08-04 20:33:01 +0000

    Carousel Initial development

--- src/components/sections/ProfileCover/ProfileCover.jsx
+++ src/components/sections/ProfileCover/ProfileCover.jsx
@@ -1,14 +1,29 @@
 import React from "react";
 import Nature from "../../../images/Nature.jpg";
+import Flowers from "../../../images/portfolio/Flowers.jpeg";
+import Greenway from "../../../images/portfolio/Greenway.jpeg";
+import Mountains from "../../../images/portfolio/Mountains.jpeg";
+import Rocks from "../../../images/portfolio/Rocks.jpeg";
+import Trees from "../../../images/portfolio/Trees.jpeg";
+import lateNight from "../../../images/portfolio/lateNight.jpeg";
+import sun from "../../../images/portfolio/sun.jpeg";
+// import Carousel from 'nuka-carousel';
 
 const ProfileCover = () => {
+  const pictures = [Nature,Flowers, Greenway, Mountains, Rocks, Trees, lateNight,sun];
   return (
     <div className="h-60 w-full">
-      <img
-        src={Nature}
-        alt="cover"
-        className="w-full h-full object-cover object-center"
-      />
+     
+      { pictures.map((picture)=>{
+       return (
+        <div className="h-10 w-full">
+         <img
+          src={picture}
+          alt="cover"
+          className="w-full h-full object-cover object-center"
+         />
+        </div>
+        )})}
     </div>
   );
 };
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: ['babel-loader']
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+            type: 'javascript/auto',
+            test: /\.mjs$/,
+            use: []
+        }
+      ]
+    }
+  };
