7ef9416008994013e823129a8349dc7eb2d6ee02
madhukarreddyganga <45326658+madhukarreddyganga@users.noreply.github.com>
2023-08-04 17:52:13 +0000

    Update ServiceItem.jsx

--- src/components/pages/Service/ServiceItem.jsx
+++ src/components/pages/Service/ServiceItem.jsx
@@ -5,10 +5,12 @@ const ServiceItem = (props) => {
   return (
     <div className="w-full lg:w-1/2">
       <div className="my-4 md:mx-4 shadow p-6 rounded-md bg-white group hover:shadow-md">
-        <div className="w-16 h-16 flex items-center justify-center rounded-md text-3xl mb-5 bg-purple-100 text-pink-600 transition duration-100 group-hover:bg-blue-900 group-hover:bg-blue-400">
+        <div className="w-16 h-16 flex items-center justify-center rounded-md text-3xl mb-5 bg-gray-900 bg-opacity-500 text-pink-600 transition duration-100 group-hover:bg-indigo-900">
           {icon}
         </div>
-        <h3 className="text-lg font-medium text-gray-800 mb-2 hover:text-indigo-400">{title}</h3>
+        <h3 className="text-lg font-medium text-gray-800 mb-2 hover:text-indigo-400">
+          {title}
+        </h3>
         <p className="text-gray-400 serviceFontDecoration">{description}</p>
       </div>
     </div>
