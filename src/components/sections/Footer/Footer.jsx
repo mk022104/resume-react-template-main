c5b93281ab0587813349a81c7d9974f16fe182be
madhukarreddyganga <45326658+madhukarreddyganga@users.noreply.github.com>
2023-08-10 20:24:23 +0000

    Added timezone variable

--- src/components/sections/Footer/Footer.jsx
+++ src/components/sections/Footer/Footer.jsx
@@ -4,6 +4,8 @@ const Footer = () => {
   const now = new Date().toLocaleTimeString();
   const Today =new Date().toDateString();
   const [time,setTime] = useState(now);
+  const timeZone = Intl.DateTimeFormat().resolvedOptions().locale;
+  console.log(timeZone);
   useEffect(()=>{
    const timer = setInterval(()=>{
       updateTime();
@@ -22,7 +24,7 @@ const Footer = () => {
     <footer className="bg-amber-950 p-4 absolute bottom-0 left-0 w-full">
       <div className="container px-4">
         <p className="text-sm text-black-900 tracking-wide text-center">
-          Designed and Developed by Madhukar Reddy.:::<span class="tracking-tight md:tracking-wide text-black-900 text-center ">{time} {Today}</span>
+          Designed and Developed by Madhukar Reddy.:::<span class="tracking-tight md:tracking-wide text-black-900 text-center ">{time} {Today} {timeZone}</span>
         </p>
       </div>
     </footer>
