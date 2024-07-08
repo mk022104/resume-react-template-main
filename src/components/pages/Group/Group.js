4a681ab2f93b13284edb04f670c607718c20fd0b
madhukarGanga <gangamadhu1234@gmail.com>
2024-07-08 12:32:15 -0400

    clean up info component

--- src/components/pages/Group/Group.js
+++ src/components/pages/Group/Group.js
@@ -6,12 +6,6 @@ import { AiOutlineInfoCircle } from "react-icons/ai";
 
 const Group =()=>{
     return (
-        // <div className="relative w-full bg-neutral-200 dark:bg-neutral-600 overflow-hidden">
-        //   <div className="mx-1 mr-14">
-        //    <img src="mountain.jpg" className="rounded-sm rounded-md rounded-lg shadow-md shadow-lg mr-6 ml-6 h-96 w-full"alt="Waterfalls"/>
-        //    <p className="text-left hover:text-center"></p>
-        //   </div>
-        // </div>
         <div className="bg-cover bg-center h-96 mr-8 ml-8 mb-8 lg:mb-8 rounded-sm rounded-md rounded-lg" style={{ backgroundImage: `url(${mountain})`  }}>
           <div className="flex items-center justify-center h-full bg-black bg-opacity-10">
              <h1 className="text-white font-serif text-left	text-center">
