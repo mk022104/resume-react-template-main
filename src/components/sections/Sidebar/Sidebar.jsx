e3747043ee759c3f19f5a776cc697f2d276aee72
mk022104 <mk022104@gmail.com>
2024-12-28 16:27:10 -0500

    Resume updated

--- src/components/sections/Sidebar/Sidebar.jsx
+++ src/components/sections/Sidebar/Sidebar.jsx
@@ -46,9 +46,9 @@ const Sidebar = () => {
           </a>
         </p>
         <a
-          href="/MadhuDevsReactResume.docx"
+          href="/MadhuResumeUpdated.docx"
           className="inline-flex mb-3 rounded bg-blue-900 text-center border-0 py-2 px-6 text-white leading-7 tracking-wide active:bg-blue-900 hover:bg-blue-900"
-          download="MadhuDevsReactResume"
+          download="MadhuResumeUpdated"
         >
         <svg class="fill-current w-4 h-7 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
           Download Resume
